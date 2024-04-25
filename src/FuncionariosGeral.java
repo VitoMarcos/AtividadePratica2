3467acda5abf737986452ce9c3f83fabe011cb04
Caetano Marsen <167832148+cabifun@users.noreply.github.com>
2024-04-24 23:17:47 -0300

    Ajustes nas funções
    Ajuste nas funções ComissaoTecnica e FuncionariosGeral.

--- src/FuncionariosGeral.java
+++ src/FuncionariosGeral.java
@@ -1,29 +1,15 @@
 public class FuncionariosGeral extends Pessoa {
 
-    String funcao;
     int tempContrato;
     float salario;
 
     public FuncionariosGeral() {}
 
-    public FuncionariosGeral(String nome, int idade, String cargo, String funcao, int tempContrato, float salario) {
+    public FuncionariosGeral(String nome, int idade, String cargo, int tempContrato, float salario) {
 
         super(nome, idade, cargo);
-        this.funcao = funcao;
         this.tempContrato = tempContrato;
         this.salario = salario;
-
-    }
-
-    public String getFuncao() {
-
-        return funcao;
-
-    }
-
-    public void setFuncao(String funcao) {
-
-        this.funcao = funcao;
 
     }
 
@@ -54,7 +40,7 @@ public class FuncionariosGeral extends Pessoa {
     @Override
     public String toString() {
 
-        return super.toString() + "\nFunção: " + funcao + "\nTempo de Contrato(anos): " + tempContrato +
+        return super.toString() + "\nTempo de Contrato(anos): " + tempContrato +
         "\nSalário: R$" + salario;
 
     }
